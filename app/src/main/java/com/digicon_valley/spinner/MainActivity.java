7b1810a9343deb60a0be883e0c729d08f635f42e
hamzagillani <hamza.gillani10@gmail.com>
2018-11-06 02:30:34 +0500

    Initial commit

---
+++ app/src/main/java/com/digicon_valley/spinner/MainActivity.java
@@ -0,0 +1,41 @@
+package com.digicon_valley.spinner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spinner=findViewById(R.id.spinner);
+        adapter=ArrayAdapter.createFromResource(this,R.array.countries_array,android.R.layout.select_dialog_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(),"Selected "+ parent.getSelectedItem(),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
+    }
+}
